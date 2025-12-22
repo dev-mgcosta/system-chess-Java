9410a8efa59707ba82cbe1f763610042f8e1a45c
Matheus G. Costa | Dev <matheuscosta.emilio@gmail.com>
2025-12-22 19:41:19 -0300

    Checkmate logic

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -22,7 +22,7 @@ public class Program {
 		List<ChessPiece> captured = new ArrayList<>();
 		
 		
-		while (true) {
+		while (!chessMatch.getCheckmate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -56,5 +56,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
